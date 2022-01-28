7f81298cde57c98c91a35edc14310f740d7326b5
elie pernet <elie.pernet@orange.fr>
2022-01-28 23:20:43 +0100

    rebase on main

--- Assets/script/colorController.cs
+++ Assets/script/colorController.cs
@@ -1,79 +1,27 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using UnityEngine;
 
 public class colorController : MonoBehaviour
 {
 	public Color color = Color.white;
-	public bool isBlack;
-	
-	public GameObject[] whitePlatforms;
-	public GameObject[] blackPlatforms;
-	public GameObject[] togglePlatforms;
 
-	public GameObject[] players;
-
-	public Sprite blackSprite;
-	public Sprite whiteSprite;
-
-	public Camera cam;
-
-	private void Start()
-	{
-		cam.backgroundColor = Color.white;
-		
-		foreach (var spriteR in togglePlatforms.Select(platform => platform.GetComponent<SpriteRenderer>()))
-		{
-			spriteR.color = Color.black;
-		}
-		
-		foreach (var spriteR in whitePlatforms.Select(platform => platform.GetComponent<SpriteRenderer>()))
-		{
-			spriteR.color = Color.white;
-		}
-		
-		foreach (var spriteR in blackPlatforms.Select(platform => platform.GetComponent<SpriteRenderer>()))
-		{
-			spriteR.color = Color.black;
-		}
-	}
+	public List<GameObject> platforms = new List<GameObject>();
 
 	public void switchColor()
 	{
-		isBlack = !isBlack;
-		
-		cam.backgroundColor = isBlack switch
+		if (color == Color.white)
 		{
-			false => Color.white,
-			true => Color.black,
-		};
-		
-		foreach (var player in players)
+			color = Color.black;
+		}
+		else if (color == Color.black)
 		{
-			var spriteR = player.GetComponent<SpriteRenderer>();
+			color = Color.white;
+		}
 
-			spriteR.sprite = isBlack switch
-			{
-				true => whiteSprite,
-				false => blackSprite,
-			};
-		}
-		
-		foreach (var spriteR in togglePlatforms.Select(platform => platform.GetComponent<SpriteRenderer>()))
+		foreach (var spriteR in platforms.Select(platform => gameObject.GetComponent<SpriteRenderer>()))
 		{
-			spriteR.color = isBlack ? Color.white : Color.black;
-		}
-		
-		foreach (var boxCollider2D in whitePlatforms.Select(platform => platform.GetComponent<BoxCollider2D>()))
-		{
-			
-		}
-		
-		foreach (var boxCollider2D in blackPlatforms.Select(platform => platform.GetComponent<BoxCollider2D>()))
-		{
-			
+			spriteR.color = color;
 		}
 	}
 }
--- Assets/script/handlebutton.cs
+++ Assets/script/handlebutton.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 public class handlebutton : MonoBehaviour
 {
 	public colorController ColorController;
-	public GameObject test;
 
 	void Start()
 	{
-		ColorController = test.GetComponent<colorController>();
+	}
+
+	void Update()
+	{
 	}
 
 	private void OnCollisionEnter2D(Collision2D col)
 	{
 		if (col.gameObject.CompareTag("player"))
 		{
-			ColorController.switchColor();
+			colorController.switchColor();
 		}
 	}
 }
