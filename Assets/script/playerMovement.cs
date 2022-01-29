0df7723595efdca4c11ec24c2f1a64efc4599225
RayaneJN <98526221+RayaneJN@users.noreply.github.com>
2022-01-29 17:42:06 +0100

    Sounds + Door

--- Assets/script/playerMovement.cs
+++ Assets/script/playerMovement.cs
@@ -8,8 +8,12 @@ public class playerMovement : MonoBehaviour
 	public Rigidbody2D rb;
 
 	public GameObject gbParticles;
-	public ParticleSystem particles;
+	private ParticleSystem particles;
 	public GameObject playerTagObject;
+
+	private AudioSource audio;
+	public AudioClip frottementSound;
+	public AudioClip sautSound;
 
 	public string horizontaleInputRef;
 	public string verticallInputRef;
@@ -32,8 +36,8 @@ public class playerMovement : MonoBehaviour
 
 	void Start()
 	{
-
 		particles = gbParticles.GetComponent<ParticleSystem>();
+		audio = gameObject.GetComponent<AudioSource>();
 	}
 
 	void Update()
@@ -70,6 +74,16 @@ public class playerMovement : MonoBehaviour
 		{
 			gbParticles.SetActive(false);
 		}
+
+		//Play Sound
+		if (Input.GetButtonDown(horizontaleInputRef) && isGrounded == true)
+		{
+			audio.Play();
+		}
+		else if (Input.GetButtonUp(horizontaleInputRef) || isGrounded == false)
+		{
+			audio.Stop();
+		}
 	}
 
 	void FixedUpdate()
@@ -91,6 +105,7 @@ public class playerMovement : MonoBehaviour
 
 	private void OnCollisionEnter2D(Collision2D col)
 	{
+		audio.PlayOneShot(sautSound);
 		// This check should be enough for simple rectangular colliders than cannot be touched
 		// from multiple directions at once (otherwise player character may "slide" from one side to another
 		// and pretend it's not on top because it collided on the side first, or reversely)
