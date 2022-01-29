eb1e4b6fd73d0b0e5fd4169ae546d6bbbeb517e4
RayaneJN <98526221+RayaneJN@users.noreply.github.com>
2022-01-29 21:34:59 +0100

    Door

--- Assets/script/doorScript.cs
+++ Assets/script/doorScript.cs
@@ -5,33 +5,72 @@ using UnityEngine;
 public class doorScript : MonoBehaviour
 {
     private int nbrPlayers;
+
+    private bool isCollisionP1;
+    private bool isCollisionP2;
+
+    public GameObject player1;
+    public GameObject player2;
     
     void Start()
     {
         nbrPlayers = 0;
+        isCollisionP1 = false;
+        isCollisionP1 = false;
     }
 
     void Update()
     {
+        if (isCollisionP1 == true && Input.GetKeyDown(KeyCode.Z))
+        {
+            player1.SetActive(false);
+            nbrPlayers += 1;
+        }
+        else if (isCollisionP2 == true && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            player2.SetActive(false);
+            nbrPlayers += 1;
+        }
+
+        if (!player1.activeSelf && Input.GetKeyDown(KeyCode.S))
+        {
+            player1.SetActive(true);
+        }
+        if (!player2.activeSelf && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            player2.SetActive(true);
+        }
+        
+        //End Game
         if (nbrPlayers == 2)
         {
             Debug.Log("Level finished");
         }
     }
     
-    void OnCollisionEnter2D(Collision2D col)
+    void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.name == "player1")
         {
-            Debug.Log("Player 1 : Level Finished");
-            col.gameObject.SetActive(false);
-            nbrPlayers += 1;
+            isCollisionP1 = true;
         }
         else if (col.gameObject.name == "player2")
         {
-            Debug.Log("Player 2 : Level Finished");
-            col.gameObject.SetActive(false);
-            nbrPlayers += 1;
+            isCollisionP2 = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.name == "player1")
+        {
+            isCollisionP1 = false;
+            nbrPlayers -= 1;
+        }
+        else if (col.gameObject.name == "player2")
+        {
+            isCollisionP2 = false;
+            nbrPlayers -= 1;
         }
     }
 }
