88b6ad59ff5c40a255f358064006336ce045754a
huulong <n.huu.long@gmail.com>
2022-01-30 14:05:58 +0100

    [DOOR] Moved door enter/exit code to player character side, inputs are now generalized and no hardcoded checks
    with the caveat that it must hide its children and deactivate its components (except Tag and playerMovement) on its own, just so the playerMovement script can process Down to exit door

--- Assets/script/doorScript.cs
+++ Assets/script/doorScript.cs
@@ -6,73 +6,49 @@ public class doorScript : MonoBehaviour
 {
     private int nbrPlayers;
 
-    private bool isCollisionP1;
-    private bool isCollisionP2;
-
-    public GameObject player1;
-    public GameObject player2;
-    
     void Start()
     {
         nbrPlayers = 0;
-        isCollisionP1 = false;
-        isCollisionP1 = false;
     }
 
-    void Update()
-    {
-        if (isCollisionP1 && Input.GetKeyDown(KeyCode.Z))
-        {
-            player1.SetActive(false);
-            nbrPlayers += 1;
-        }
-        else if (isCollisionP2 && Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            player2.SetActive(false);
-            nbrPlayers += 1;
-        }
-
-        if (!player1.activeSelf && Input.GetKeyDown(KeyCode.S))
-        {
-            player1.SetActive(true);
-            nbrPlayers -= 1;
-        }
-        if (!player2.activeSelf && Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            player2.SetActive(true);
-            nbrPlayers -= 1;
-        }
-        
-        //End Game
-        if (nbrPlayers == 2)
-        {
-            Debug.Log("Level finished");
-        }
-    }
-    
     void OnTriggerEnter2D(Collider2D col)
     {
-        switch (col.gameObject.name)
+        var playerMovement = col.GetComponent<playerMovement>();
+        if (playerMovement)
         {
-            case "player1":
-                isCollisionP1 = true;
-                break;
-            case "player2":
-                isCollisionP2 = true;
-                break;
+            playerMovement.RegisterNearbyDoor(this);
         }
     }
 
     void OnTriggerExit2D(Collider2D col)
     {
-        switch (col.gameObject.name)
+        var playerMovement = col.GetComponent<playerMovement>();
+        if (playerMovement)
         {
-            case "player1":
-                isCollisionP1 = false;
-                break;
-            case "player2":
-                isCollisionP2 = false;
-                break;
+            playerMovement.UnregisterNearbyDoor(this);
+        }
+    }
+
+    public void MakeCharacterEnterDoor(playerMovement playerMovementScript)
+    {
+        playerMovementScript.OnEnterDoor(this);
+        nbrPlayers += 1;
+
+        CheckCharactersInDoor();
+    }
+
+    public void MakeCharacterExitDoor(playerMovement playerMovementScript)
+    {
+        playerMovementScript.OnExitDoor(this);
+        nbrPlayers -= 1;
+    }
+
+    private void CheckCharactersInDoor()
+    {
+        //End Game
+        if (nbrPlayers == 2)
+        {
+            Debug.Log("Level finished");
         }
     }
 }
--- Assets/script/playerMovement.cs
+++ Assets/script/playerMovement.cs
@@ -7,16 +7,24 @@ using UnityEngine.Serialization;
 public class playerMovement : MonoBehaviour
 {
 	public Rigidbody2D rb;
-	
+	public SpriteRenderer spriteRenderer;
+	public Collider2D myCollider2D;
+
 	public ParticleSystem particles;
 	public GameObject playerTagObject;
+
+	[Tooltip("Player number: should be 1 or 2")]
+	public int playerNumber;
 
 	[FormerlySerializedAs("horizontaleInputRef")]
 	public string horizontalInputRef;
 
 	[FormerlySerializedAs("verticallInputRef")]
-	public string verticalInputRef;
-	private AudioSource audio;
+	public string moveUpInputRef;
+
+	public string moveDownInputRef;
+
+	private AudioSource audioSource;
 	public AudioClip frottementSound;
 	public AudioClip sautSound;
 
@@ -34,24 +42,31 @@ public class playerMovement : MonoBehaviour
 
 	private float wantedHorizontalSpeed;
 
-	private List<Collider2D> currentGrounds = new List<Collider2D>();
+	private readonly List<Collider2D> currentGrounds = new List<Collider2D>();
 	private bool IsGrounded => currentGrounds.Count > 0;
 
-	private bool isJumping;
+	private bool wantsToGoUp;
+	private bool wantsToGoDown;
 
-	
+	private doorScript nearbyDoor;
+	private doorScript enteredDoor;
+
 	void Start()
 	{
-		audio = gameObject.GetComponent<AudioSource>();
+		audioSource = gameObject.GetComponent<AudioSource>();
 	}
 
 	void Update()
 	{
 		wantedHorizontalSpeed = Input.GetAxis(horizontalInputRef) * moveSpeed;
 
-		if (Input.GetButtonDown(verticalInputRef) && IsGrounded)
+		if (Input.GetButtonDown(moveUpInputRef))
 		{
-			isJumping = true;
+			wantsToGoUp = true;
+		}
+		else if (Input.GetButtonDown(moveDownInputRef))
+		{
+			wantsToGoDown = true;
 		}
 
 		//PLayer & particles rotation + PlayerTag flip
@@ -79,50 +94,84 @@ public class playerMovement : MonoBehaviour
 		{
 			particles.gameObject.SetActive(false);
 		}
-
-		//Play Sound
-		if (Input.GetButtonDown(horizontalInputRef) && IsGrounded)
-		{
-			audio.Play();
-		}
-		else if (Input.GetButtonUp(horizontalInputRef) || !IsGrounded)
-		{
-			audio.Stop();
-		}
 	}
 
 	void FixedUpdate()
 	{
-		MovePlayer(wantedHorizontalSpeed);
+		if (enteredDoor)
+		{
+			HandleActionsInsideDoor();
+		}
+		else
+		{
+			MovePlayer();
+
+			//Play Sound
+			if (!audioSource.isPlaying)
+			{
+				if (wantedHorizontalSpeed != 0 && IsGrounded)
+				{
+					audioSource.Play();
+				}
+			}
+			else
+			{
+				if (wantedHorizontalSpeed == 0 || !IsGrounded)
+				{
+					audioSource.Stop();
+				}
+			}
+		}
 	}
 
-	void MovePlayer(float _wantedHorizontalSpeed)
+	void MovePlayer()
 	{
 		var oldVelocity = rb.velocity;
 
 		// Quickly reach target velocity X
-		float newVelocityX = Mathf.SmoothDamp(oldVelocity.x, _wantedHorizontalSpeed, ref velocityXDampingCurrentVelocity, .05f);
+		float newVelocityX = Mathf.SmoothDamp(oldVelocity.x, wantedHorizontalSpeed, ref velocityXDampingCurrentVelocity, .05f);
 
-		float newVelocityY;
+		float newVelocityY = oldVelocity.y;
 
-		if (isJumping)
+		if (wantsToGoUp)
 		{
-			// Instantly apply jump velocity on Y
-			newVelocityY = initialJumpSpeed;
-			isJumping = false;
-		}
-		else
-		{
-			// Keep last velocity Y
-			newVelocityY = oldVelocity.y;
+			wantsToGoUp = false;
+
+			// Can only jump or enter door if grounded
+			if (IsGrounded)
+			{
+				if (nearbyDoor)
+				{
+					// Door nearby, enter it instead of jumping
+					nearbyDoor.MakeCharacterEnterDoor(this);
+				}
+				else
+				{
+					// Instantly apply jump velocity on Y
+					newVelocityY = initialJumpSpeed;
+				}
+			}
 		}
 
 		rb.velocity = new Vector2(newVelocityX, newVelocityY);
 	}
 
+	void HandleActionsInsideDoor()
+	{
+		Debug.Assert(enteredDoor != null, "Missing entered door, yet we should be inside door", this);
+
+		if (wantsToGoDown)
+		{
+			wantsToGoDown = false;
+
+			// Exit door we are in
+			enteredDoor.MakeCharacterExitDoor(this);
+		}
+	}
+
 	private void OnCollisionEnter2D(Collision2D col)
 	{
-		audio.PlayOneShot(sautSound);
+		audioSource.PlayOneShot(sautSound);
 		// This check should be enough for simple rectangular colliders than cannot be touched
 		// from multiple directions at once (otherwise player character may "slide" from one side to another
 		// and pretend it's not on top because it collided on the side first, or reversely)
@@ -141,5 +190,94 @@ public class playerMovement : MonoBehaviour
 		{
 			currentGrounds.Remove(other.collider);
 		}
+	}
+
+	public void RegisterNearbyDoor(doorScript door)
+	{
+		if (nearbyDoor == door)
+		{
+			Debug.LogWarningFormat("Nearby door {0} already registered on player {1}, nothing to do!",
+				nearbyDoor, playerNumber);
+			return;
+		}
+
+		if (nearbyDoor)
+		{
+			Debug.LogWarningFormat("Different nearby door {0} already registered on player {1}, new door {2} will replace it!",
+				nearbyDoor, playerNumber, door);
+		}
+
+		nearbyDoor = door;
+	}
+
+	public void UnregisterNearbyDoor(doorScript door)
+	{
+		if (!nearbyDoor)
+		{
+			Debug.LogWarningFormat("No nearby door registered on player {1}, no nearby door to unregister!",
+				playerNumber);
+			return;
+		}
+
+		if (nearbyDoor != door)
+		{
+			Debug.LogWarningFormat("Different nearby door registered on player {0} is {1}, instead of expected {2}. We will NOT unregister {1}!",
+				playerNumber, nearbyDoor, door);
+			return;
+		}
+
+		nearbyDoor = null;
+	}
+
+	public void OnEnterDoor(doorScript door)
+	{
+		if (enteredDoor == door)
+		{
+			Debug.LogWarningFormat("Entered door {0} already registered on player {1}, nothing to do!",
+				enteredDoor, playerNumber);
+			return;
+		}
+
+		if (enteredDoor)
+		{
+			Debug.LogWarningFormat("Different entered door {0} already registered on player {1}, new door {2} will replace it!",
+				enteredDoor, playerNumber, door);
+		}
+
+		enteredDoor = door;
+
+		// If you add new components, make sure to deactivate them here, except the Tag and this very playerMovement so we can still process Down to exit door
+		spriteRenderer.enabled = false;
+		rb.simulated = false;
+		myCollider2D.enabled = false;
+		audioSource.Stop();
+
+		particles.gameObject.SetActive(false);
+	}
+
+	public void OnExitDoor(doorScript door)
+	{
+		if (!enteredDoor)
+		{
+			Debug.LogWarningFormat("No entered door registered on player {1}, no entered door to unregister!",
+				playerNumber);
+			return;
+		}
+
+		if (enteredDoor != door)
+		{
+			Debug.LogWarningFormat("Different entered door registered on player {0} is {1}, instead of expected {2}. We will NOT unregister {1}!",
+				playerNumber, enteredDoor, door);
+			return;
+		}
+
+		enteredDoor = null;
+
+		// If you add new components, make sure to reactivate them here, except the Tag and this very playerMovement
+		spriteRenderer.enabled = true;
+		rb.simulated = true;
+		myCollider2D.enabled = true;
+
+		particles.gameObject.SetActive(true);
 	}
 }
