9653cf0fc774ccf1b63dc292d3497bf113a07368
huulong <n.huu.long@gmail.com>
2023-09-12 21:14:19 +0200

    [Texture] Reduce texture sizes tremendously are they were much bigger than necessary, using player character sprite 5984 -> 256px as reference
    This makes a rescale of factor of 0.04278. We then moved sprites to children if necessary and rescaled them by the inverse factor 23.375, to restore original size as seen in game.
    Traps are made of 3 sprites and many instances of traps, as well as a few instances of buttons, were not proper prefab instances so we replaced them. In the process we also simplified coloring reference: we now get Component In Children or even Components (plural) In Children for neutral platforms, so spikes children sprites can be made invisible too.

--- Assets/script/colorController.cs
+++ Assets/script/colorController.cs
@@ -44,17 +44,17 @@ public class colorController : MonoBehaviour
 	{
 		cam.backgroundColor = Color.white;
 
-		foreach (var spriteR in togglePlatforms.Select(platform => platform.GetComponent<SpriteRenderer>()))
+		foreach (var spriteR in togglePlatforms.Select(platform => platform.GetComponentInChildren<SpriteRenderer>()))
 		{
 			spriteR.color = Color.black;
 		}
 
-		foreach (var spriteR in whitePlatforms.Select(platform => platform.GetComponent<SpriteRenderer>()))
+		foreach (var spriteR in whitePlatforms.Select(platform => platform.GetComponentInChildren<SpriteRenderer>()))
 		{
 			spriteR.color = Color.white;
 		}
 
-		foreach (var spriteR in blackPlatforms.Select(platform => platform.GetComponent<SpriteRenderer>()))
+		foreach (var spriteR in blackPlatforms.Select(platform => platform.GetComponentInChildren<SpriteRenderer>()))
 		{
 			spriteR.color = Color.black;
 		}
@@ -83,7 +83,7 @@ public class colorController : MonoBehaviour
 
 		foreach (var player in players)
 		{
-			var spriteR = player.GetComponent<SpriteRenderer>();
+			var spriteR = player.GetComponentInChildren<SpriteRenderer>();
 
 			spriteR.sprite = isBlack ? whiteSprite : blackSprite;
 
@@ -100,7 +100,7 @@ public class colorController : MonoBehaviour
 
 		foreach (var door in doors)
 		{
-			var spriteR = door.GetComponent<SpriteRenderer>();
+			var spriteR = door.GetComponentInChildren<SpriteRenderer>();
 
 			spriteR.sprite = isBlack switch
 			{
@@ -109,9 +109,13 @@ public class colorController : MonoBehaviour
 			};
 		}
 
-		foreach (var spriteR in togglePlatforms.Select(platform => platform.GetComponent<SpriteRenderer>()))
+		// ! There may be multiple sprites per object here, e.g. trap spike sprites
+		foreach (var spriteRenderers in togglePlatforms.Select(platform => platform.GetComponentsInChildren<SpriteRenderer>()))
 		{
-			spriteR.color = isBlack ? Color.white : Color.black;
+			foreach (SpriteRenderer spriteR in spriteRenderers)
+			{
+				spriteR.color = isBlack ? Color.white : Color.black;
+			}
 		}
 
 		foreach (var platform in blackPlatforms)
